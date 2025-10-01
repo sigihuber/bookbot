036f2c1d4205ff746ef91a7f5e17b3717cdf1e00
sigihuber <sigi.huber@me.com>
2025-10-01 21:06:04 +0200

    add sorting and report function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, sort_num_chars
 
 
 def main():
@@ -6,13 +6,26 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
-    print(f"Found {num_words} total words")
-    print(num_chars)
+    sorted_chars = sort_num_chars(num_chars)
+    generate_report(book_path, num_words, sorted_chars)
 
 
+# Returns the content of a text file as a string
 def get_book_text(path: str) -> str:
     with open(path) as f:
         return f.read()
+
+
+# Generate Report
+def generate_report(book_path, num_words, sorted_chars):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book fond at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,8 +1,10 @@
+# Returns the number of words in the text.
 def get_num_words(text: str) -> int:
     words = text.split()
     return len(words)
 
 
+# Returns the number of characters in the text.
 def get_num_chars(text: str) -> dict[str, int]:
     num_chars: dict[str, int] = {}
     for char in text:
@@ -12,3 +14,18 @@ def get_num_chars(text: str) -> dict[str, int]:
         else:
             num_chars[c] = 1
     return num_chars
+
+
+# The function takes a dictionary and returns the value of the "num" key.
+# This is how the `.sort()` method knows how to sort the list of dictionaries.
+def sort_on(items):
+    return items["num"]
+
+
+# Takes a dictionary of character counts and returns a sorted list of dictionaries.
+def sort_num_chars(num_chars):
+    char_list = []
+    for char in num_chars:
+        char_list.append({"char": char, "num": num_chars[char]})
+    char_list.sort(key=sort_on, reverse=True)
+    return char_list
